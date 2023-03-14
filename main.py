cab3001155795984b3ea42cefdb6bb49d51ec194
juliusace9000 <79270157+juliusace9000@users.noreply.github.com>
2023-03-14 14:31:54 +0100

    fertig
    Es wird alles in eine feather datei geschrieben und duplikate entfernt

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import http.client
 from xml.etree import cElementTree as ElementTree
 from XML import *
 import pandas as pd
-
+import time
 clientid = "52e03cb779352cb1a4665642fdca25fe"
 apikey = "28d386dd30329b4a7975ec458409c424"
 # clientid = "4b20783637b74f5655ca2363f31f5594"
 # apikey = "14ad2eeee362b17212cd4ce382cd2453"
 
 
-EVANO = "8002549" #Ahrensburg = 8000446, Winsen = 8006484, Ham HBF = 8002549
+EVANO = "8000446" #Ahrensburg = 8000446, Winsen = 8006484, Ham HBF = 8002549
 DATE = "230314"#YYMMDD
 HOUR = "13" #HH
 
@@ -60,53 +60,47 @@ def getComChg(evano):
     root = ElementTree.XML(data.decode("utf-8"))
     return XmlDictConfig(root)
 
-delays = getComChg(EVANO)
-delays = pd.json_normalize(delays['s'])
-#print(delays[['dp.l','dp.m','ar.m.t','ar.m.c','ar.m.ts']])
-#print(delays[['dp.pp','dp.ppth','dp.pt','m.from','m.to']])
-#print(delays['dp.pp'], delays['dp.ppth'], delays['dp.pt'], delays['m.from'], delays['m.to'])
-# print("\n\nPLAN:")
-print(delays)
-plan = None
-for hour in range(0,24):
-    try:
-        planHour = getPlan(DATE, str(hour).zfill(2), EVANO)
-    except:
-        continue
-    try:
-        planHour = pd.json_normalize(planHour['s'])
-        planHour = drop(['ar.pde', 'dp.pde', 'tl.f', 'tl.t', 'tl.n', 'tl.c', 'tl.o', 'ar.ppth', 'dp.ppth', 'ar.pp'], planHour)
-        plan = pd.concat([plan, planHour])
-    except:
-        pass
-plan.to_csv('plan.csv')
-print(plan)
+while True:
 
-# ids = []
-# for id in plan['id']:
-#     print(id)
-#     ids.append(id)
-# i = 0
-# table = pd.DataFrame()
-# for d_id, d_ar_ct, d_ar_l, d_dp_ct, d_dp_l in zip(delays['id'], delays['ar.ct'], delays['ar.l'], delays['dp.ct'], delays['dp.l']):
-#     print(d_dp_l)
-#     for p_id, p_ar_pt, p_ar_l, p_dp_pt, p_dp_pp, p_dp_l in zip(plan['id'], plan['ar.pt'], plan['ar.l'], plan['dp.pt'], plan['dp.pp'], plan['dp.l']):
-#         if p_id == d_id:
-#             try:
-#                 d = str(int(d_dp_ct)-int(p_dp_pt))
-#             except:
-#                 d = 'canceled'
-#             if d_dp_l != d_dp_l:
-#                 line = p_dp_l
-#             else:
-#                 line = d_dp_l
-#             if line != line:
-#                 continue
-#             temp = pd.DataFrame([[d_id, p_dp_pt,      d_dp_ct,        line,   p_dp_pp,    d]],
-#                         columns=['id',  'dep. plan', 'dep. cur.', 'line', 'platform', 'delay'],
-#                         index=[i])
-#             table=pd.concat([table, temp])
-#             i+=1
-# table.sort_values('dep. plan')
-# table.reset_index()
-# print(table)
+    min = time.strftime("%M", time.localtime())
+    hour = time.strftime("%H", time.localtime())
+    if int(time.strftime("%M", time.localtime())) % 5 == 0 or True:
+        try:
+            table = None
+            table = pd.read_feather("feather")
+        except:
+            table = None
+            table = pd.DataFrame()
+        delays = getComChg(EVANO)
+        delays = pd.json_normalize(delays['s'])
+        plan = getPlan(str(time.strftime("%Y%m%d", time.localtime()))[2:], str(hour).zfill(2), EVANO)
+        plan = pd.json_normalize(plan['s'])
+        
+        for p_id, p_ar_pt, p_ar_l, p_dp_pt, p_dp_pp, p_dp_l in zip(plan['id'], plan['ar.pt'], plan['ar.l'], plan['dp.pt'], plan['dp.pp'], plan['dp.l']):
+            for d_id, d_ar_ct, d_ar_l, d_dp_ct, d_dp_l in zip(delays['id'], delays['ar.ct'], delays['ar.l'], delays['dp.ct'], delays['dp.l']):
+                if p_id == d_id:
+                    try:
+                        d = str(int(d_dp_ct)-int(p_dp_pt))
+                    except:
+                        d = 'canceled'
+                    if d_dp_l != d_dp_l:
+                        line = p_dp_l
+                    else:
+                        line = d_dp_l
+                    if line != line:
+                        continue
+                    if line == "SEV":
+                        continue
+                    temp = pd.DataFrame([[d_id, int(p_dp_pt),      d_dp_ct,        line,   p_dp_pp,    d]],
+                                columns=['id',  'dep. plan', 'dep. cur.', 'line', 'platform', 'delay'])
+                    table=pd.concat([table, temp])
+                    
+        print(table.shape)
+        table.drop_duplicates(subset=['dep. plan'], keep='last')
+        print(table.shape)
+        print(table['dep. plan'])
+        table.sort_values('dep. plan')
+        table = table.reset_index()
+        table.to_feather("table")
+        time.sleep(30)
+    time.sleep(30)
